89c7a1f623dc45744661df9c5ba7824d318a1a95
Paepcke, Michael <git@paepcke.de>
2023-01-03 07:21:19 +0000

    auto: sync upstream / update dependencies

--- walker_fast.go
+++ walker_fast.go
@@ -1,8 +1,5 @@
-//go:build windows
+//go:build !windows
 
-// walker windows does not implement (yet) skiping hardline
-// files(inode seen based on windows 'fileid') and no support
-// for filesystem boundary crossing checks
 package codescore
 
 import (
@@ -20,11 +17,16 @@ func (c *Config) fastWalker() {
 	if err != nil {
 		errExit("[stat deviceID root dir] [" + c.Path + "] [" + err.Error() + "]")
 	}
+	d, ok := fi.Sys().(*syscall.Stat_t)
+	if !ok {
+		errExit("[stat deviceID root dir] [" + c.Path + "]")
+	}
 	exclude := false
 	if len(c.Exclude) > 0 {
 		exclude = true
 	}
 	skipme := false
+	rootNodeDeviceID := uint64(d.Dev)
 	for i := 0; i < 1; i++ {
 		go func() {
 			inodeSeen := make(map[uint64]struct{})
@@ -45,10 +47,19 @@ func (c *Config) fastWalker() {
 					}
 					ftype := uint32(item.Type())
 					name := path + "/" + fname
+					inode := fi.Sys().(*syscall.Stat_t).Ino
+					if _, ok := inodeSeen[inode]; ok {
+						continue // skip inode if we seen it already
+					}
+					inodeSeen[inode] = struct{}{}
 					if ftype&_modeSymlink != 0 {
 						continue // skip symlinks
 					}
 					if ftype&_modeDir != 0 {
+						st, _ := fi.Sys().(*syscall.Stat_t)
+						if uint64(st.Dev) != rootNodeDeviceID {
+							continue // skip dirtargets outside fs boundary
+						}
 						if exclude {
 							skipme = false
 							for _, exclude := range c.Exclude {
